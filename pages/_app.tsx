6b268b795b1918ecbfb57af9cdd76588dfc3686e
Pablo Henrique <pablo_hen@hotmail.com>
2021-09-08 12:15:40 -0300

    config redux-persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,19 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { Provider } from 'react-redux';
 import store from './../store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  let persistor = persistStore(store);
+
   return (
     <Provider store={store}>
-      <ThemeProvider attribute="class" defaultTheme="light">
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
