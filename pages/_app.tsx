47d203a3fb028c113423d13e40ad893be5796dca
Pablo Henrique <pablo_hen@hotmail.com>
2021-09-09 11:58:06 -0300

    use AuthGuard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,16 @@ import { ThemeProvider } from 'next-themes';
 import { Provider } from 'react-redux';
 import { store, persistor } from '../store/';
 import { PersistGate } from 'redux-persist/integration/react';
+import AuthGuard from '../components/AuthGuard';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider attribute="class" defaultTheme="light">
-          <Component {...pageProps} />
+          <AuthGuard>
+            <Component {...pageProps} />
+          </AuthGuard>
         </ThemeProvider>
       </PersistGate>
     </Provider>
