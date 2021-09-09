1f6645116af69860fddf0f75e67a77903a6c1bda
Pablo Henrique <pablo_hen@hotmail.com>
2021-09-09 12:00:32 -0300

    creatinue implementation

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
 
 interface Props {
   children: any;
 }
 
 const AuthGuard = ({ children }: Props) => {
-  const router = useRouter();
   const accessToken = useSelector((state: any) => state.user.accessToken);
 
-  return (
-    <div>
-      {!accessToken && router.push('/')}
-      {children}
-    </div>
-  );
+  if (!accessToken && window.location.pathname !== '/login') {
+    window.location.pathname = '/login';
+  }
+
+  if (accessToken && window.location.pathname === '/login') {
+    window.location.pathname = '/terminais';
+  }
+
+  return children;
 };
 
 export default AuthGuard;
