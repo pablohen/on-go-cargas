6eda2c133d0f7c0ba43e088dda108ca59dd0d9da
Pablo Henrique <pablo_hen@hotmail.com>
2021-09-09 14:04:57 -0300

    layout changes

--- components/OnGoContainer.tsx
+++ components/OnGoContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const OnGoContainer = ({ children }: Props) => {
   return (
-    <div className="w-full p-8 rounded-xl bg-gray-100/70 dark:bg-gray-600/70 backdrop-blur-lg shadow-lg space-y-4">
+    <div className="w-full p-4 rounded-xl bg-gray-100/80 dark:bg-gray-600/70 backdrop-blur-lg shadow-lg space-y-4">
       {children}
     </div>
   );
